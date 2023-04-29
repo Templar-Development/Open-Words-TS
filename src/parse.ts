47fa8eb8ce715e7797495457dd90c6beab748de4
cqb13 <cqb13.main@gmail.com>
2023-04-29 11:27:52 -0400

    checkStems

--- src/parse.ts
+++ src/parse.ts
@@ -6,17 +6,18 @@ import Inflects from "./data/inflects";
 
 class Parse {
   constructor(
-    wordsDict = WordsDict,
-    addons = LatinAddons,
-    stems = Stems,
-    uniques = Uniques,
-    inflects = Inflects
+    private wordsDict = WordsDict,
+    private addons = LatinAddons,
+    private stems = Stems,
+    private uniques = Uniques,
+    private inflects = Inflects
   ) {
+
     // Sort by length
-    stems.sort((a: any, b: any) => a.length - b.length);
+    this.stems.sort((a: any, b: any) => a.length - b.length);
 
     // Sort by length of ending
-    inflects.sort((a: any, b: any) => a.ending.length - b.ending.length);
+    this.inflects.sort((a: any, b: any) => a.ending.length - b.ending.length);
   }
 
   public parseLine(line: any, direction: "lte" | "etl", formatted: boolean): any{
@@ -75,7 +76,7 @@ class Parse {
 
 
     // check against list of uniques
-    for (const unique of Uniques) {
+    for (const unique of this.uniques) {
       if (word == unique.orth) {
         out.push({ "w": unique, "stems": [] });
         isUnique = true;
@@ -105,23 +106,66 @@ class Parse {
      * the end of the word string and then check the resulting stem
      * against the list of stems from stemList.ts
      */
-
+  
     let matchStems: any = [];
-
+  
     // For each of the inflections that is a match, strip the inflection from the end of the word
     // Then look up the stripped word (w) in the stems
     for (const infl of infls) {
-
+      const w = s.replace(new RegExp(`${infl.ending}$`), '');
+  
+      for (const stem of this.stems) {
+        if (w === stem.orth) {
+          // If the inflection and stem identify as the same part of speech
+          if (
+            infl.pos === stem.pos ||
+            (infl.pos === 'VPAR' && stem.pos === 'V')
+          ) {
+            // Ensure the inflections apply to the correct stem decl/conj/etc
+            if (infl.n[0] === stem.n[0]) {
+              let isInMatchStems = false;
+  
+              // If this stem is already in the match_stems list, add infl to that stem (if not already an infl in that stem list)
+              for (let i = 0; i < matchStems.length; i++) {
+                const mst = matchStems[i];
+                if (stem === mst.st) {
+                  isInMatchStems = true;
+  
+                  // So the matches a stem in the match_stems. Is it unique to that stem's infls. If so, append it to that stem's infls.
+                  let isInStemInfls = false;
+                  for (const stemInfl of mst.infls) {
+                    if (stemInfl.form === infl.form) {
+                      isInStemInfls = true;
+                      // we found a match, stop looking
+                      break;
+                    }
+                  }
+  
+                  if (!isInStemInfls) {
+                    mst.infls.push(infl);
+                  }
+                }
+              }
+  
+              if (!isInMatchStems) {
+                matchStems.push({ st: stem, infls: [infl] });
+              }
+            }
+          }
+        }
+      }
     }
+  
+    return matchStems;
   }
-
+  
   private splitEnclitic(s: string): any {
     //Split enclitic ending from word
 
     let out: any = [];
 
     // Test the different tackons / packons as specified in addons.ts
-    for (const e of LatinAddons.tackons) {
+    for (const e of this.addons.tackons) {
       if (s.endsWith(e.orth)) {
 
         // Standardize data format
@@ -137,7 +181,7 @@ class Parse {
     }
 
     if (s.startsWith("qu")) {
-      for (const e of LatinAddons.packons) {
+      for (const e of this.addons.packons) {
         if (s.endsWith(e.orth)) {
           out.push({ 'w': e });
           s = s.replace(new RegExp(e.orth + "$"), "");
@@ -145,7 +189,7 @@ class Parse {
         }
       }
     } else {
-      for (const e of LatinAddons.notPackons) {
+      for (const e of this.addons.notPackons) {
         if (s.endsWith(e.orth)) {
           out.push({ 'w': e });
           s = s.replace(new RegExp(e.orth + "$"), "");
