a80678673b4369f738e5c78ba1150fb27fe0845d
cqb13 <cqb13.main@gmail.com>
2023-05-03 21:39:58 -0400

    Moved roman numeral translation to latinToEnglish

--- src/parser.ts
+++ src/parser.ts
@@ -12,15 +12,43 @@ import Uniques from "./data/uniques";
 import Inflects from "./data/inflects";
 
 class Parser {
-  stems: ({ pos: string; form: string; orth: string; n: number[]; wid: number; } | { pos: string; form: string; orth: string; n: string[]; wid: number; })[];
-  inflects: { ending: string; pos: string; note: string; n: (string | number)[]; form: string; }[];
-  uniques: { orth: string; pos: string; senses: string[]; }[];
+  stems: (
+    | { pos: string; form: string; orth: string; n: number[]; wid: number }
+    | { pos: string; form: string; orth: string; n: string[]; wid: number }
+  )[];
+  inflects: {
+    ending: string;
+    pos: string;
+    note: string;
+    n: (string | number)[];
+    form: string;
+  }[];
+  uniques: { orth: string; pos: string; senses: string[] }[];
   //TODO: Add type for addons
   addons: any;
-  wordsDict: ({ pos: string; n: number[]; parts: string[]; senses: string[]; form: string; orth: string; id: number; } | { pos: string; n: string[]; parts: string[]; senses: string[]; form: string; orth: string; id: number; })[];
+  wordsDict: (
+    | {
+        pos: string;
+        n: number[];
+        parts: string[];
+        senses: string[];
+        form: string;
+        orth: string;
+        id: number;
+      }
+    | {
+        pos: string;
+        n: string[];
+        parts: string[];
+        senses: string[];
+        form: string;
+        orth: string;
+        id: number;
+      }
+  )[];
   formatter: Formatter;
   tricks: Tricks;
-  
+
   constructor() {
     this.stems = Stems;
     this.inflects = Inflects;
@@ -35,17 +63,21 @@ class Parser {
     this.inflects.sort((a: any, b: any) => a.ending.length - b.ending.length);
 
     // Support classes
-    this.formatter = new Formatter()
+    this.formatter = new Formatter();
     this.tricks = new Tricks();
   }
 
-  public parseLine(line: any, direction: "lte" | "etl", formatted: boolean): any {
+  public parseLine(
+    line: any,
+    direction: "lte" | "etl",
+    formatted: boolean
+  ): any {
     // Prase a line of words delimitated by spaces
 
     let out = [];
 
     line = this.formatter.sanitize(line);
-    let words = line.split(' ');
+    let words = line.split(" ");
 
     for (let i = 0; i < words.length; i++) {
       let word = words[i];
@@ -57,10 +89,14 @@ class Parser {
     return out;
   }
 
-  public parse(inputString: string, direction: "lte" | "etl", formatted: boolean): any {
+  public parse(
+    inputString: string,
+    direction: "lte" | "etl",
+    formatted: boolean
+  ): any {
     /**
      * Parse an input string as a Latin word and look it up in the Words dictionary.
-     * 
+     *
      * Return dictionary and grammatical data formatted in a similar manner as original
      * Words program.
      */
@@ -70,27 +106,19 @@ class Parser {
     let s = inputString;
 
     //do lookup based on parse direction
-    if (direction === 'lte') {
+    if (direction === "lte") {
       out = this.latinToEnglish(s);
-    } else if (direction === 'etl'){
+    } else if (direction === "etl") {
       out = this.englishToLatin(s);
     } else {
       throw new Error("Invalid direction");
-    }
-
-    if (this.tricks.onlyRomanDigits(s)) {
-      let num = this.tricks.evaluateRomanNumerals(s);
-
-      if (num != 0) {
-        out.push({ "w": { "orth": s, "pos": "INT", "n": [num], "parts": [], "senses": [num], "id": `${-1}` }, "stems": [] });
-      }
     }
 
     if (formatted) {
       out = this.formatter.formatOutput(out);
     }
 
-    return { "word": s, "defs": out }
+    return { word: s, defs: out };
   }
 
   private latinToEnglish(word: string): any {
@@ -103,11 +131,10 @@ class Parser {
     word = split[0];
     out = split[1];
 
-
     // check against list of uniques
     for (const unique of this.uniques) {
       if (word == unique.orth) {
-        out.push({ "w": unique, "stems": [] });
+        out.push({ w: unique, stems: [] });
         isUnique = true;
         break;
       }
@@ -117,7 +144,7 @@ class Parser {
     if (!isUnique) {
       out = this.findForms(word, false);
     }
-    
+
     // TODO: handle this in findForms, to improve speed
     // Some words that start with i can also start with j
     // ex: iecit -> jecit
@@ -126,23 +153,42 @@ class Parser {
       out = this.findForms(word, false);
     }
 
+    // translate roman numerals
+    if (this.tricks.onlyRomanDigits(word)) {
+      let num = this.tricks.evaluateRomanNumerals(word);
+
+      if (num != 0) {
+        out.push({
+          w: {
+            orth: word,
+            pos: "INT",
+            n: [num],
+            parts: [],
+            senses: [num],
+            id: `${-1}`
+          },
+          stems: []
+        });
+      }
+    }
+
     return out;
   }
 
   private englishToLatin(word: string): any {
     // Find Latin word from English definition
     let out: any = [];
-  
+
     // Check against list of uniques
     for (const unique of this.uniques) {
       for (const sense of unique.senses) {
         if (sense.toLowerCase().includes(word.toLowerCase())) {
-          out.push({ "w": unique, "stems": [] });
+          out.push({ w: unique, stems: [] });
           break;
         }
       }
     }
-  
+
     // If it's not in the list of uniques
     if (out.length === 0) {
       out = this.lookupWord(word);
@@ -150,14 +196,14 @@ class Parser {
 
     return out;
   }
-  
+
   private lookupWord(word: string): any {
     let out: any = [];
-  
+
     for (const dictLine of this.wordsDict) {
       for (const sense of dictLine.senses) {
         if (sense.toLowerCase().includes(word.toLowerCase())) {
-          out.push({ "w": dictLine, "stems": [] });
+          out.push({ w: dictLine, stems: [] });
           break;
         }
       }
@@ -168,16 +214,16 @@ class Parser {
 
     return out;
   }
-  
+
   private getStems(form: string, pos: string): any {
     let stems: any = [];
-  
+
     for (const stem of this.stems) {
       if (stem.pos === pos && stem.form === form) {
         stems.push(stem);
       }
     }
-  
+
     return stems;
   }
 
@@ -188,7 +234,6 @@ class Parser {
     // Check against list of inflections
     for (const infl of this.inflects) {
       if (s.endsWith(infl.ending)) {
-
         // If the longest inflection has been found, stop looking
         if (infls.length > 0 && infls[0].ending.length > infl.ending.length) {
           break;
@@ -209,7 +254,7 @@ class Parser {
     } else {
       out = this.lookupStems(stems, out);
     }
-    
+
     if (out.length === 0 && !reduced) {
       let rOut: any = this.reduce(s);
       // if there is more data after reducing, extend out
@@ -230,18 +275,18 @@ class Parser {
     let infls = [];
 
     // For each inflection match, check prefixes and suffixes
-    for (const prefix of this.addons['prefixes']) {
-      if (s.startsWith(prefix['orth'])) {
-        s = s.replace(`^${prefix['orth']}`, '');
-        out.push({ w: prefix, stems: [], addon: 'prefix' });
+    for (const prefix of this.addons["prefixes"]) {
+      if (s.startsWith(prefix["orth"])) {
+        s = s.replace(`^${prefix["orth"]}`, "");
+        out.push({ w: prefix, stems: [], addon: "prefix" });
         break;
       }
     }
 
-    for (const suffix of this.addons['suffixes']) {
-      if (s.endsWith(suffix['orth'])) {
-        s = s.replace(`${suffix['orth']}$`, '');
-        out.push({ w: suffix, stems: [], addon: 'suffix' });
+    for (const suffix of this.addons["suffixes"]) {
+      if (s.endsWith(suffix["orth"])) {
+        s = s.replace(`${suffix["orth"]}$`, "");
+        out.push({ w: suffix, stems: [], addon: "suffix" });
         break;
       }
     }
@@ -250,7 +295,7 @@ class Parser {
 
     // Has reducing input string given us useful data?
     for (const word of out) {
-      if (word['stems'].length > 0) {
+      if (word["stems"].length > 0) {
         foundNewMatch = true;
       }
     }
@@ -274,14 +319,14 @@ class Parser {
     // For each of the inflections that is a match, strip the inflection from the end of the word
     // Then look up the stripped word (w) in the stems
     for (const infl of infls) {
-      const w = s.replace(new RegExp(`${infl.ending}$`), '');
+      const w = s.replace(new RegExp(`${infl.ending}$`), "");
 
       for (const stem of this.stems) {
         if (w === stem.orth) {
           // If the inflection and stem identify as the same part of speech
           if (
             infl.pos === stem.pos ||
-            (infl.pos === 'VPAR' && stem.pos === 'V')
+            (infl.pos === "VPAR" && stem.pos === "V")
           ) {
             // Ensure the inflections apply to the correct stem decl/conj/etc
             if (infl.n[0] === stem.n[0]) {
@@ -333,41 +378,44 @@ class Parser {
           let wordIsInOut = false;
           for (let i = 0; i < out.length; i++) {
             const w = out[i];
-            if ('id' in w['w'] && word['id'] === w['w']['id'] || w['w']['orth'] === word['orth']) {
+            if (
+              ("id" in w["w"] && word["id"] === w["w"]["id"]) ||
+              w["w"]["orth"] === word["orth"]
+            ) {
               // It is in the out list already, flag and then check if the stem is already in the stems
               wordIsInOut = true;
-  
+
               // Ensure the stem is not already in the out word stems
               let wordIsInOutWordStems = false;
-              for (const st of out[i]['stems']) {
+              for (const st of out[i]["stems"]) {
                 if (st === stem) {
                   wordIsInOutWordStems = true;
                   // We have a match, break the loop
                   break;
                 }
               }
-  
+
               if (!wordIsInOutWordStems) {
-                out[i]['stems'].push(stem);
+                out[i]["stems"].push(stem);
               }
               // If we matched a word in the out, break the loop
               break;
             }
-            }
-            let tempStem = stem;
-            // If the word isn't in the out yet
-            if (!wordIsInOut) {
-              // Check the VPAR / V relationship
-              if (word['pos'] === 'V') {
-                // If the stem doesn't match the 4th principle part, it's not VPAR
-                if (word['parts'].indexOf(stem['st']['orth']) === 3) {
-                  // Remove "V" infls
-                  tempStem = this.removeExtraInfls(stem, 'V');
-                } else {
-                  // Remove "VPAR" infls
-                  tempStem = this.removeExtraInfls(stem, 'VPAR');
-                }
+          }
+          let tempStem = stem;
+          // If the word isn't in the out yet
+          if (!wordIsInOut) {
+            // Check the VPAR / V relationship
+            if (word["pos"] === "V") {
+              // If the stem doesn't match the 4th principle part, it's not VPAR
+              if (word["parts"].indexOf(stem["st"]["orth"]) === 3) {
+                // Remove "V" infls
+                tempStem = this.removeExtraInfls(stem, "V");
+              } else {
+                // Remove "VPAR" infls
+                tempStem = this.removeExtraInfls(stem, "VPAR");
               }
+            }
             out.push({ w: { ...word }, stems: [tempStem] });
           }
         }
@@ -384,13 +432,12 @@ class Parser {
     // Test the different tackons / packons as specified in addons.ts
     for (const e of this.addons.tackons) {
       if (s.endsWith(e.orth)) {
-
         // Standardize data format
         e.form = e.orth;
 
         // Est exception
         if (s != "est") {
-          out.push({ 'w': e, "stems": [] });
+          out.push({ w: e, stems: [] });
           s = s.replace(new RegExp(e.orth + "$"), "");
         }
         break;
@@ -400,7 +447,7 @@ class Parser {
     if (s.startsWith("qu")) {
       for (const e of this.addons.packons) {
         if (s.endsWith(e.orth)) {
-          out.push({ 'w': e });
+          out.push({ w: e });
           s = s.replace(new RegExp(e.orth + "$"), "");
           break;
         }
@@ -408,7 +455,7 @@ class Parser {
     } else {
       for (const e of this.addons.notPackons) {
         if (s.endsWith(e.orth)) {
-          out.push({ 'w': e });
+          out.push({ w: e });
           s = s.replace(new RegExp(e.orth + "$"), "");
           break;
         }
